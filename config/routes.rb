73a0b3641870741b46024ba3b438ebe7ff949a71
Odinakachukwu Ezeobika <solomonzbk@gmail.com>
2019-09-17 18:37:55 +0100

    Add session features

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :room_messages
+  resources :rooms
+  root controller: :rooms, action: :index
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
